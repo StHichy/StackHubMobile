9bb678a6319053e91b6836a97bdf237b994185ac
Carlos Henrique <stormhichy@gmail.com>
2025-08-14 09:49:41 -0300

    first commit

---
+++ componentes/container.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+
+    container:{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "red",
+        width: "100%",
+
+
+    }
+
+
+})
+
+
+
+
+
+export default style;
